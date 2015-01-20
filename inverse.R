ccfc2ef3d2eaca117f01cdb2be7878cc7784a201
HarbinLc2014 <lc1826442304@sina.com>
2015-01-20 16:28:15 +0800

    函数完成

--- inverse.R
+++ inverse.R
@@ -1,7 +1,24 @@
-makeCacheMatrix<-function(x = matrix()){
-                    ##get the matrix
+makeCacheMatrix <- function(x = matrix()) {
+        a <- NULL
+        set <- function(y) {
+                x <<- y
+                a <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) a <<- solve
+        getsolve <- function() a
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
-
-cacheSolve<- function(x, ...){
-                    ##return the inverse matrix
+cacheSolve <- function(x, ...) {
+        a <- x$getsolve()
+        if(!is.null(a)) {
+                message("getting cached data")
+                return(a)
+        }
+        data <- x$get()
+        a <- solve(data, ...)
+        x$setsolve(a)
+        a
 }
